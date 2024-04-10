b90f9ade6b956eb74369e9e69add82dcb68d5aa9
Anuj Rishu TIwari <at6272@srmist.edu.in>
2024-04-11 05:08:53 +0530

    final code

--- src/Components/Random.js
+++ src/Components/Random.js
@@ -1,31 +1,39 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import Spinner from './Spinner';
 
+const API_KEY = process.env.REACT_APP_GIPHY_API_KEY || 'Xr0MBP9JBTftSYWFkfMLC70Sq5dgTxA7';
 
-const API_KEY = process.env.REACT_APP_GIPHY_API_KEY
 const Random = () => {
-    const[ gif  ,setGif] =useState('')
-     async function fetchData() {
-        const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`; // Corrected string formatting
-        const output = await axios.get(url)
-console.log(output)
-   }
+    const [gif, setGif] = useState('')
+    const [loading, setLoading] = useState(false)
 
-   useEffect(()=>{
-    fetchData();
-   },[])
-    function clickHandler(){
-
+    async function fetchData() {
+        setLoading(true); // Start loading
+        const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+        const output = await axios.get(url);
+        const imageSource = output.data.data.images.downsized_large.url;
+        setGif(imageSource);
+        setLoading(false); // End loading
     }
 
+    useEffect(() => {
+        fetchData();
+    }, [])
 
-  return (
-    <div className='w-1/2 h-[450px] bg-blue-400 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]' >
-   <h1 className='text-2xl underline uppercase font-bold' >Random Gif </h1>
-   <img width="450" src={gif} alt="gif" />
-   <button className='w-10/12 bg-blue-700 text-lg rounded-lg text-white' onClick={clickHandler} >Genrate</button>
-    </div>
-  )
+    function clickHandler() {
+        fetchData();
+    }
+
+    return (
+        <div className='w-1/2  bg-blue-400 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]'>
+            <h1 className='text-2xl mt-[15px] underline uppercase font-bold'>Random Gif </h1>
+            {
+                loading ? (<Spinner />) : (<img width="400" src={gif} alt="gif" />)
+            }
+            <button className='w-10/12 mb-[20px] bg-blue-700 text-lg rounded-lg text-white' onClick={clickHandler}>Generate</button>
+        </div>
+    )
 }
 
 export default Random
